2a39427958f0423597313d82efdb2b2cccc2aa7e
0x9334 <a>
2025-06-25 10:52:00 +0700

    add preserved models for user friendly run

---
+++ crypto_models/preseved_models.py
@@ -0,0 +1,12 @@
+PRESERVED_MODELS = {
+    "qwen3-embedding-0.6b": "bafkreiacd5mwy4a5wkdmvxsk42nsupes5uf4q3dm52k36mvbhgdrez422y",
+    "qwen3-1.7b": "bafkreib6pws5dx5ur6exbhulmf35twfcizdkxvup4cklzprlvaervfz5zy",
+    "qwen3-4b": "bafkreiekokvzioogj5hoxgxlorqvbw2ed3w4mwieium5old5jq3iubixza",
+    "qwen3-8b": "bafkreid5z4lddvv4qbgdlz2nqo6eumxwetwmkpesrumisx72k3ahq73zpy",
+    "qwen3-14b": "bafkreiclwlxc56ppozipczuwkmgnlrxrerrvaubc5uhvfs3g2hp3lftrwm",
+    "qwen3-32b": "bafkreihq4usl2t3i6pqoilvorp4up263yieuxcqs6xznlmrig365bvww5i",
+    "qwen3-30b-a3b": "bafkreieroiopteqmtbjadlnpq3qkakdu7omvtuavs2l2qbu46ijnfdo2ly",
+    "gemma3-4b": "bafkreiaevddz5ssjnbkmdrl6dzw5sugwirzi7wput7z2ttcwnvj2wiiw5q",
+    "gemma3-12b": "bafkreic2bkjuu3fvdoxnvusdt4in6fa6lubzhtjtmcp2zvokvfjpyndakq",
+    "gemma3-27b": "bafkreihi2cbsgja5dwa5nsuixicx2x3gbcnh7gsocxbmjxegtewoq2syve",
+}
