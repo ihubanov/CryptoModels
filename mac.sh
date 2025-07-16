19e92be918f37875498168494d87b2ea21f16930
0x9334 <a>
2025-07-16 15:24:22 +0700

    update tag for mac.sh

--- mac.sh
+++ mac.sh
@@ -286,7 +286,7 @@ show_package_status() {
 #   MLX_FLUX_TAG="feature-xyz"   # Install from specific branch
 
 MLX_FLUX_TAG="1.0.2"           # Leave empty for latest, or set specific tag
-CRYPTOMODELS_TAG="1.1.15"   # Leave empty for latest, or set specific tag
+CRYPTOMODELS_TAG="1.1.16"   # Leave empty for latest, or set specific tag
 
 # Uncomment the lines below to see available tags before installation
 # list_available_tags "https://github.com/0x9334/mlx-flux.git" "mlx-flux"
