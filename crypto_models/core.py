4a1bb8e45e5816c57067f57b622089421d98a41d
0x9334 <a>
2025-07-24 16:02:03 +0700

    fixed: update lora config

--- crypto_models/core.py
+++ crypto_models/core.py
@@ -1761,7 +1761,7 @@ class CryptoModelsManager:
             if model_info.get("lora_config", None) is None:
                 return False
             model_info["lora_config"] = {}
-            for key, value in request["lora_items"].items():
+            for key, value in request["lora_config"].items():
                 model_info["lora_config"][key] = {
                     "path": value["path"],
                     "scale": value["scale"]
